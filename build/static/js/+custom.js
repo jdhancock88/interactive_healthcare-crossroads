b71363bb7ed8b602ec3d13b47b497353554c4707
jdhancock88 <jdhancock@gmail.com>
2017-03-22 16:45:05 -0500

    Adds intro screen to and redesigns individual pages.

--- build/static/js/+custom.js
+++ build/static/js/+custom.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
+/*
+global $:true, window $:true
+*/
 
-	//custom scripting goes here
+setTimeout(() => {
+  $('.overlay__container').removeClass('init--display');
+}, 1000);
 
-
+$(window).scroll(() => {
+  if ($(window).scrollTop() > 10) {
+    $('#health-overlay').fadeOut(1000);
+  } else {
+    $('#health-overlay').fadeIn(1000);
+  }
 });
